dc1230f8b807b289df7d54dc803ba03b1d213ffa
laksh-krishna-sharma <asklakshsharma@gmail.com>
2025-03-08 21:32:39 +0530

    ANN

--- lecture1/PT/densedLayer2.py
+++
@@ -1,7 +0,0 @@
-import torch as tc
-
-layer = tc.nn.Linear(in_features=3, out_features=2)
-
-inputs = tc.randn(5, 3)  # Batch of 5 samples, each with 3 features
-output = layer(inputs)
-print(output)
--- lecture1/TF/densedLayer2.py
+++
@@ -1,7 +0,0 @@
-import tensorflow as tf
-
-layer = tf.keras.layers.Dense(units=2, activation="sigmoid")
-
-inputs = tf.random.normal((5, 3))  # Batch of 5 samples, each with 3 features
-output = layer(inputs)
-print(output)
